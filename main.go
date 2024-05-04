52c72ff8109e8c60aef44256fea8201a0063bbcc
SkobelkinYaroslav <yarl@duck.com>
2024-05-04 19:42:28 +0400

    updated hash func, fixed bug in output

--- main.go
+++ main.go
@@ -4,29 +4,32 @@ import (
 	"bytes"
 	"errors"
 	"fmt"
+	"hash/fnv"
 	"io"
 	"log"
 	"os"
 	"time"
 )
 
-func parseChunk(data []byte, mp map[string]*res) {
+const (
+	maxWorkStation = 10000
+)
+
+func parseChunk(data []byte, cm *customMap) {
 	prev := 0
 	for i := 0; i < len(data); i++ {
 		if data[i] == '\n' {
 			name, val := parseString(data[prev:i])
 			prev = i + 1
 
-			data, ok := mp[string(name)]
-			if !ok {
-				data = &res{}
-				mp[string(name)] = data
+			arrayData := cm.get(name)
+
+			if arrayData == nil {
+				arrayData = &node{}
 			}
 
-			data.min = min(data.min, val)
-			data.max = max(data.max, val)
-			data.sum += val
-			data.count++
+			cm.put(name, val)
+
 		}
 	}
 }
@@ -64,6 +67,54 @@ type res struct {
 	min, sum, max, count int32
 }
 
+type node struct {
+	key []byte
+	res
+	next *node
+}
+
+type customMap []*node
+
+func (c *customMap) put(key []byte, val int32) {
+	index := hash(key) % maxWorkStation
+	nd := (*c)[index]
+
+	if nd == nil {
+		(*c)[index] = &node{key: key, res: res{min: val, max: val, sum: val, count: 1}}
+	} else {
+		for nd.next != nil && !bytes.Equal(key, nd.key) {
+			nd = nd.next
+		}
+
+		if bytes.Equal(key, nd.key) {
+			nd.res = res{min: min(nd.min, val), max: max(nd.max, val), sum: nd.sum + val, count: nd.count + 1}
+		} else {
+			nd.next = &node{key: key, res: res{min: val, max: val, sum: val, count: 1}}
+		}
+	}
+}
+
+func (c *customMap) get(key []byte) *node {
+	index := hash(key) % maxWorkStation
+	nd := (*c)[index]
+
+	for nd != nil && !bytes.Equal(key, nd.key) {
+		nd = nd.next
+	}
+
+	if nd != nil {
+		return nd
+	}
+
+	return nil
+}
+
+func hash(s []byte) int {
+	h := fnv.New32a()
+	h.Write(s)
+	return int(h.Sum32())
+}
+
 func main() {
 	defer func(t time.Time) {
 		fmt.Println(time.Since(t))
@@ -80,7 +131,7 @@ func main() {
 	buf := make([]byte, chunkSize)
 	leftOver := make([]byte, 0, chunkSize)
 
-	mp := make(map[string]*res)
+	mp := make(customMap, maxWorkStation)
 
 	for {
 		n, err := file.Read(buf)
@@ -104,12 +155,18 @@ func main() {
 		leftOver = make([]byte, len(buf[lastNewLineIndex+1:]))
 		copy(leftOver, buf[lastNewLineIndex+1:])
 
-		parseChunk(toSend, mp)
+		parseChunk(toSend, &mp)
 
 	}
 
-	for k, v := range mp {
-		fmt.Printf("%s: min: %.1f, max: %.1f, avg: %.1f\n", k, float32(v.min)/10, float32(v.max)/10, float32(v.sum/v.count)/10)
+	for _, item := range mp {
+		if item != nil {
+			fmt.Printf("%s: min: %.1f, max: %.1f, avg: %.1f\n", item.key, float32(item.min)/10, float32(item.sum/item.count)/10, float32(item.max)/10)
+			for item.next != nil {
+				item = item.next
+				fmt.Printf("%s: min: %.1f, max: %.1f, avg: %.1f\n", item.key, float32(item.min)/10, float32(item.max)/10, float32(item.sum/item.count)/10)
+			}
+		}
 	}
 
 }
